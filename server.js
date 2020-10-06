5d196ce46e8dc93036b3c7c0dcab55f077ed080e
Nathanael Shim <nathanaelshim@Nathanaels-Mac-mini.local>
2020-10-06 12:14:11 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+require('dotenv').config({ path: '.env' });
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const Pusher = require('pusher');
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
+  useTLS: true,
+});
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// API calls
+app.post('/update-editor', (req, res) => {
+  pusher.trigger('editor', 'code-update', {
+   ...req.body,
+  });
+
+  res.status(200).send('OK');
+});
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+app.listen(port, () => console.log(`Listening on port ${port}`));
